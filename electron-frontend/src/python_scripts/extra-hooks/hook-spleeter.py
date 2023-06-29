fa281335e33f641a25c6a463b4ea33fb8814e726
malwaregarry <malwaregarry@gmail.com>
2023-06-29 16:23:26 +0800

    Update pyinstaller hooks for demucs

--- electron-frontend/src/python_scripts/extra-hooks/hook-spleeter.py
+++
@@ -1,4 +0,0 @@
-from PyInstaller.utils.hooks import collect_data_files, collect_submodules, collect_dynamic_libs
-datas = collect_data_files('spleeter')
-hiddenimports = collect_submodules('spleeter')
-binaries = collect_dynamic_libs('spleeter')
